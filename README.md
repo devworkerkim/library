273d6db313eb261e438de77730563689631a6b44
Kim Leung <71901468+devworkerkim@users.noreply.github.com>
2021-09-05 15:56:46 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Library - The Odin Project
+
+This is the repo for the Library project from the JavaScript course of The Odin Project
+
+## What I Learned
+
+This project was an exercise working with objects in JavaScript.  My experience with objects has been mainly working with object literals or other objects that have their prototypes already defined.  This was my first time creating objects using a constructor function.  My previous learning was using classes, which I didn't find very intuitive and according to the articles provided throughout the course has been nothing more than syntactic sugar to appease the masses who come from different languages.  While objects seemed pretty common in JavaScript, I had no experience working beyond just assigning key-pair values and passing them around.
+
+I feel a bit more comfortable now working with objects and creating them from scratch to suit my data needs.  The articles and videos provided great explanations on what the prototype is and how it affects the instances of those objects.  The visuals were key in describing how objects work and operate in JavaScript.
+
+I didn't use `object.create()` in this project since I had started a constructor function from the last lesson.  It made more sense in the time being to continue creating `new` objects.
+
+## Optional Local Storage
+
+I had worked with `localStorage` before, and since things were moving along I figured I would implement it.  I was rudely reminded by the error messages in my console that `localStorage` uses DOMstrings as their format of choice.  There was some messing around, but I finally figured out how to utilize `JSON.stringify()` and `JSON.parse()` to move between `localStorage` and the browser window's working memory.
+
+The instructions did hint that there would be an issue with object methods not being transferred to `localStorage` since it uses DOMstrings.  I felt it best to keep not only a copy of the data in `localStorage` but to continue maintaining a copy in working memory.  This meant I only had reinstate the object methods upon first load of the page for the data stored that doesn't have the object methods.  Anything else created from the page will automatically have an instance saved in working memory to operate on.
---
+++ script.js
@@ -0,0 +1,100 @@
+if (!localStorage.getItem('myLibrary')) {
+    localStorage.setItem('myLibrary', JSON.stringify([
+        {
+          "title": "Eloquent JavaScript",
+          "author": "Marijn Haverbeke",
+          "pages": 472,
+          "read": true
+        },
+        {
+          "title": "JavaScript: The Good Parts",
+          "author": "Douglas Crockford",
+          "pages": 176,
+          "read": false
+        },
+        {
+          "title": "A Smarter Way To Learn JavaScript",
+          "author": "Mark Myers",
+          "pages": 256,
+          "read": true
+        },
+        {
+          "title": "The Pragmatic Programmer",
+          "author": "Andrew Hunt & David Thomas",
+          "pages": "352",
+          "read": false
+        }
+      ]));
+}
+
+let myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+myLibrary.forEach(function (book, ind) {
+    myLibrary[ind] = new Book(book.title, book.author, book.pages, book.read);
+});
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages`;
+    }
+    this.readToggle = function() {
+        this.read = !this.read;
+    }
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+function displayLibrary(library) {
+    while (document.querySelector('.library').firstChild) {
+        document.querySelector('.library').removeChild(document.querySelector('.library').firstChild);
+    }
+    library.forEach(function (book, ind) {
+        const readBtn = document.createElement('button');
+        readBtn.innerText = book.read ? 'I HAVE read this book' : 'I have NOT read this book';
+        readBtn.style.backgroundColor = book.read ? 'green' : 'yellow';
+        readBtn.onclick = function () {
+            myLibrary[ind].readToggle();
+            localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+            displayLibrary(myLibrary);
+        }
+        const removeBtn = document.createElement('button');
+        removeBtn.innerText = 'Remove';
+        removeBtn.onclick = function () {
+            myLibrary.splice(ind, 1);
+            localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+            displayLibrary(myLibrary);
+        };
+        const paragraph = document.createElement('p');
+        paragraph.innerText = book.info();
+        const div = document.createElement('div');
+        div.classList.add('book');
+        div.appendChild(paragraph);
+        div.appendChild(readBtn);
+        div.appendChild(removeBtn);
+        document.querySelector('.library').appendChild(div);
+    });
+}
+
+document.querySelector('#addBook').addEventListener('click', function() {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const read = document.querySelector('#read').checked;
+    if (title && author && pages) {
+        addBookToLibrary(new Book(title, author, pages, read));
+        localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+        displayLibrary(myLibrary);
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#pages').value = '';
+        document.querySelector('#read').checked = false;
+    }
+});
+
+displayLibrary(myLibrary);
